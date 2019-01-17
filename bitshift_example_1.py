1b40c9d3fa71bb573856278db6b1015d51f53ad7
SC\paul.craven <paul@cravenfamily.com>
2019-01-17 13:06:56 -0600

    Add examples from physical layer.

---
+++ bitshift_example_1.py
@@ -0,0 +1,24 @@
+# This program prints out the bits in a number.
+
+# How many bits should we encode. Usually 8, or some other multiple of 8
+bits_to_encode = 8
+
+# What number do we want to encode?
+number_to_encode = 23
+
+# You can also encode letters by using ord(), which fetches
+# the value of the letter.
+# number_to_encode = ord('X')
+
+# Loop for each bit
+for bit_pos in range(bits_to_encode):
+
+	# Use a single 1, and bit shift it with << to the
+	# spot we are interested in.
+	bit = (1 << bit_pos) & number_to_encode
+	if bit != 0:
+		bit_value = 1
+	else:
+		bit_value = 0
+
+	print("Bit position {:2} is {} which is worth {:2}".format(bit_pos, bit_value, bit))
---
+++ bitshift_example_2.py
@@ -0,0 +1,29 @@
+# How many bits should we encode. Usually 8, or some other multiple of 8
+bits_to_encode = 8
+
+# The b means this is a byte array.
+# A byte array is different than a string. A letter in a string
+# can be represented by multiple bytes. Here it can't. And this
+# can have values other than letters if we really wanted.
+byte_array = b'This is a message.'
+
+# Loop through each byte in the array
+for my_byte in byte_array:
+
+    # Now pull each bit out of the letter.
+	for bit_pos in range(bits_to_encode):
+
+		bit = 1 << bit_pos & my_byte
+		if bit != 0:
+			bit_value = 1
+		else:
+			bit_value = 0
+
+		# IMPORTANT: We are printing the least significant bit first.
+		# This basically means our number is printed out BACKWARDS.
+		# Printing it out forwards would mean extra code that would confuse
+		# things, so I'm leaving it like this.
+		print(bit_value, end="")
+
+	# Done with this letter. Go to the next line.
+	print(" - {:3} - {:}".format(my_byte, chr(my_byte)))
