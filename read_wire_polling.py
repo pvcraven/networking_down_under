8ffcc832bd5328cb3b3fd745b00ef4d8ab92d920
Paul Vincent Craven <paul@cravenfamily.com>
2019-02-02 11:05:06 -0600

    Use constants instead of hard-coded values.

--- read_wire_polling.py
+++ read_wire_polling.py
@@ -1,13 +1,16 @@
 import time
 import RPi.GPIO as GPIO
 
+GPIO_CHANNEL = 12
+TIME_DELAY = 0.25
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.IN)
+GPIO.setup(GPIO_CHANNEL, GPIO.IN)
 
 while True:
 
     # Read from pin 12. We'll get a 0 or a 1.
-    result = GPIO.input(12)
+    result = GPIO.input(GPIO_CHANNEL)
 
     if result:
         # If true (1), then print high
@@ -17,4 +20,4 @@ while True:
         print("Low")
 
     # Wait a quarter second before we look again
-    time.sleep(0.25)
+    time.sleep(TIME_DELAY)
