65c524197cec8959b89466034d35454a237512b1
Paul Vincent Craven <paul@cravenfamily.com>
2019-04-22 09:46:07 -0500

    Add ansi graphics examples

---
+++ ansi_test_asciimatics.py
@@ -0,0 +1,22 @@
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+
+
+def demo(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("NETWORKING", font='big'),
+            screen.height // 2 - 8),
+        Cycle(
+            screen,
+            FigletText("DOWN UNDER", font='big'),
+            screen.height // 2 + 3),
+        Stars(screen, (screen.width + screen.height) // 2)
+    ]
+    screen.play([Scene(effects, 500)])
+
+
+Screen.wrapper(demo)
---
+++ ansi_test_basic.py
@@ -0,0 +1,41 @@
+# Enable ANSI graphics for Windows DOS prompt
+import colorama
+colorama.init()
+
+# Define colors
+# \x says there is a hex number coming.
+# 1B in hex is an "escape" character.
+# [ starts the sequence, and m ends it.
+# 31 is for red. 32 green.
+# 0 will reset everything.
+# 1 will bold the text.
+BRIGHT_RED = "\x1b[1;31m"
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+RESET = "\x1b[0m"
+
+# Clear the screen
+CLEAR_SCREEN = "\x1b[2J"
+
+# Special ANSI characters you can draw boxes with
+BOTTOM_RIGHT_BOX = "\x1b(0\x6a\x1bB("
+TOP_RIGHT_BOX = "\x1b(0\x6b\x1bB("
+TOP_LEFT_BOX = "\x1b(0\x6c\x1bB("
+BOTTOM_LEFT_BOX = "\x1b(0\x6d\x1bB("
+CROSS = "\x1b(0\x6e\x1bB("
+HORIZONTAL = "\x1b(0\x71\x1bB("
+VERTICAL = "\x1b(0\x78\x1bB("
+
+# Print some colored text
+print(CLEAR_SCREEN, end="")
+print("Adding colors is {}great{} to do.".format(BRIGHT_RED, RESET))
+print("This {}is in green{}.".format(GREEN, RESET))
+
+# Print a table of ANSI characters
+for i in range(0x21, 0x7e):
+	if i % 8 == 0:
+		print()
+	char = chr(i)
+	print("0x{:0x} \x1b(0{}\x1b(B".format(i, char), end="  ")
+
+print()
