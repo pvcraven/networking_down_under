1b95447a6db444a19105414ad85f0169e98733b5
Paul Vincent Craven <paul@cravenfamily.com>
2020-01-24 10:00:21 -0600

    Clarify Java send data example,

--- SendData.java
+++ SendData.java
@@ -5,20 +5,20 @@ import java.net.Socket;
 public class SendData {
 
     public static void main(String[] args) throws Exception {
+
         InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
         int serverPort =  10000;
+        String myData = "Hello world!";
 
-        System.out.println("Connecting...");
         Socket socket = new Socket(serverAddress, serverPort);
 
-        System.out.println("Connected, sending data...");
-
-        String myData = "Hello world!";
         boolean autoFlush = true;
         PrintWriter out = new PrintWriter(socket.getOutputStream(), autoFlush);
         out.print(myData);
         out.flush();
 
-        System.out.println("Done");
+        socket.close();
+
+        System.out.println("Done sending message.");
     }
 }
