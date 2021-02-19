a9d7bf179e2d9a68de4ab3cbcc8c29ed59464827
Paul Vincent Craven <paul@cravenfamily.com>
2021-02-19 13:46:41 -0600

    Update

--- manchester_encoding.py
+++ manchester_encoding.py
@@ -37,10 +37,16 @@ for my_byte in my_message:
     # Starting at 7 down to 0
     for bit_pos in range(BITS_IN_A_BYTE - 1, -1, -1):
 
-        # Take a 1 and shift into our bit position.
-        # Compare to the data, any value left means we have a 1 in that position
-        # 0100 0000
-
+        # Use a single 1, and bit shift it with << to the
+        # spot we are interested in. Then use a bitwise and (&)
+        # and to see if that spot has a value.
+        # if bit pos = 6 then
+        #   1 << 6 = 0100 0000 = 64
+        # if my_byte = 'H' which ASCII 72 = 0100 1000 binary
+        # H      = 0100 1000
+        # 1 << 6 = 0100 0000
+        #          ---------
+        # &      = 0100 0000 = 64 (result of bit-wise 'and')
         bit = (1 << bit_pos) & my_byte
 
         # A 1 could be 1, 2, 4, 8, 16, 32, 64, or 128 position
